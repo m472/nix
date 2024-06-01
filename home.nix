c71778e578cca2f92add418a5b8de4901aca28a8
Mathias Graf <mathias.n.graf@gmail.com>
2024-06-01 10:10:48 +0200

    qutebrowser hints customized

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, environment, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ ./neovim.nix ./hyprland.nix ];
@@ -28,120 +28,122 @@
     };
   };
 
-  programs.ssh = {
-    enable = true;
-    addKeysToAgent = "yes";
-  };
-
   services.ssh-agent.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName = "Mathias Graf";
-    userEmail = "mathias.n.graf@gmail.com";
-    extraConfig = {
-      core = {
-        editor = "nvim";
-        pager = "delta";
-      };
-      merge.tool = "meld";
-      init.defaultBranch = "main";
-      push.autoSetupRemote = "true";
-      interactive.diffFilter = "delta --color-only";
-      delta = {
-        navigate = true;
-        light = false;
-      };
+  programs = {
+    ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
     };
-  };
 
-  programs.qutebrowser = {
-    enable = true;
-    keyBindings = {
-      normal = {
-        "J" = "tab-prev";
-        "K" = "tab-next";
+    git = {
+      enable = true;
+      userName = "Mathias Graf";
+      userEmail = "mathias.n.graf@gmail.com";
+      extraConfig = {
+        core = {
+          editor = "nvim";
+          pager = "delta";
+        };
+        merge.tool = "meld";
+        init.defaultBranch = "main";
+        push.autoSetupRemote = "true";
+        interactive.diffFilter = "delta --color-only";
+        delta = {
+          navigate = true;
+          light = false;
+        };
       };
     };
-    extraConfig = ''
-      c.zoom.default = '75%'
-      c.aliases["chromium"] = "spawn --detach chromium --app={url} --disable-extensions;; tab-close"
-    '';
 
-    searchEngines = {
-      py = "https://pypi.org/search/?q={}";
-      wiki =
-        "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
-      np =
-        "https://search.nixos.org/packages?channel=23.11&from=0&size=50&sort=relevance&type=packages&query={}";
+    qutebrowser = {
+      enable = true;
+      keyBindings = {
+        normal = {
+          "J" = "tab-prev";
+          "K" = "tab-next";
+        };
+      };
+      extraConfig = ''
+        c.zoom.default = '75%'
+
+        # shortcut to open a tab as a chromium app and close it in qutebrowser
+        c.aliases["chromium"] = "spawn --detach chromium --app={url} --disable-extensions;; tab-close"
+
+        # adapt hint color to hyprland style
+        c.hints.border = '1px solid #262626'
+        c.colors.hints.match.fg = 'white'
+        c.colors.hints.match.fg = 'white'
+        c.colors.hints.bg = 'qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 rgba(51, 204, 255, 0.8), stop:1 rgba(0, 255, 153, 0.8))'
+      '';
     };
-  };
 
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
-    '';
-    shellAbbrs = {
-      nv = "nvim";
-      Ga = "git add";
-      Gap = "git add -p";
-      Gc = "git commit";
-      Gcm = "git commit -m";
-      Gd = "git diff";
-      Gds = "git diff --staged";
-      Gs = "git status";
-      Gl = "git log";
-      Gpl = "git pull";
-      Gph = "git push";
-      sl = "ls";
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
+      '';
+      shellAbbrs = {
+        nv = "nvim";
+        Ga = "git add";
+        Gap = "git add -p";
+        Gc = "git commit";
+        Gcm = "git commit -m";
+        Gd = "git diff";
+        Gds = "git diff --staged";
+        Gs = "git status";
+        Gl = "git log";
+        Gpl = "git pull";
+        Gph = "git push";
+        sl = "ls";
+      };
     };
-  };
 
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-    options = [ "--cmd cd" ];
-  };
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+      options = [ "--cmd cd" ];
+    };
 
-  programs.kitty = {
-    enable = true;
-    font.name = "FiraCode-Regular";
-    font.size = 10;
-    settings = {
-      scrollback_lines = 40000;
-      font_features = "FiraCode-Regular +ss05";
-      enable_audio_bell = false;
-      window_padding_width = "2 5";
+    kitty = {
+      enable = true;
+      font.name = "FiraCode-Regular";
+      font.size = 10;
+      settings = {
+        scrollback_lines = 40000;
+        font_features = "FiraCode-Regular +ss05";
+        enable_audio_bell = false;
+        window_padding_width = "2 5";
 
-      # Gruvbox Dark theme, author: Pavel Pertsev, license: MIT/X11
-      selection_foreground = "#ebdbb2";
-      selection_background = "#d65d0e";
+        # Gruvbox Dark theme, author: Pavel Pertsev, license: MIT/X11
+        selection_foreground = "#ebdbb2";
+        selection_background = "#d65d0e";
 
-      background = "#282828";
-      foreground = "#ebdbb2";
+        background = "#282828";
+        foreground = "#ebdbb2";
 
-      color0 = "#3c3836";
-      color1 = "#cc241d";
-      color2 = "#98971a";
-      color3 = "#d79921";
-      color4 = "#458588";
-      color5 = "#b16286";
-      color6 = "#689d6a";
-      color7 = "#a89984";
-      color8 = "#928374";
-      color9 = "#fb4934";
-      color10 = "#b8bb26";
-      color11 = "#fabd2f";
-      color12 = "#83a598";
-      color13 = "#d3869b";
-      color14 = "#8ec07c";
-      color15 = "#fbf1c7";
+        color0 = "#3c3836";
+        color1 = "#cc241d";
+        color2 = "#98971a";
+        color3 = "#d79921";
+        color4 = "#458588";
+        color5 = "#b16286";
+        color6 = "#689d6a";
+        color7 = "#a89984";
+        color8 = "#928374";
+        color9 = "#fb4934";
+        color10 = "#b8bb26";
+        color11 = "#fabd2f";
+        color12 = "#83a598";
+        color13 = "#d3869b";
+        color14 = "#8ec07c";
+        color15 = "#fbf1c7";
 
-      cursor = "#bdae93";
-      cursor_text_color = "#665c54";
+        cursor = "#bdae93";
+        cursor_text_color = "#665c54";
 
-      url_color = "#458588";
+        url_color = "#458588";
+      };
     };
   };
 }
