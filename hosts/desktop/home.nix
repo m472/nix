09150beb8f500ec5b25b5a0d1c4ec32d0dc6e0c1
Mathias Graf <mathias.n.graf@gmail.com>
2026-09-22 21:00:42 +0200

    desktop shikane config fixed

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -15,7 +15,6 @@ _: {
           search = [
             "m=ASUS VS247"
             "s=D6LMTF079392"
-            "v="
           ];
           position = "1920,0";
           mode = "1920x1080@60Hz";
@@ -25,7 +24,6 @@ _: {
           search = [
             "m=ASUS VS247"
             "s=D6LMTF079389"
-            "v="
           ];
           position = "0,0";
           mode = "1920x1080@60Hz";
